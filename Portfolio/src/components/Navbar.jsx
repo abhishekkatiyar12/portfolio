b07083c92d8f312d0ea9afde7d5e3cbfebb6af7e
Abhishek <abhishek.katiyar.983932@gmail.com>
2025-09-09 19:06:48 +0530

    removed experienced from navbar

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
   const menuItems = [
     { id: "about", label: "About" },
     { id: "skills", label: "Skills" },
-    { id: "experience", label: "Experience" },
+    // { id: "experience", label: "Experience" },
     { id: "work", label: "Projects" },
     { id: "education", label: "Education" },
   ];
